36c7ff84865136825edb90bfc5fc6fa41462bf27
pattonsgirl <pattonsgirl@gmail.com>
2020-01-24 14:14:49 -0500

    added definition of main & preprocessor directives

--- Examples/scanf_printf_variables.c
+++ Examples/scanf_printf_variables.c
@@ -1,3 +1,6 @@
+//These are preprocessor directives
+//  Before program is compiled, it will use #include to grab the library
+//  And #define to define "global" variables
 #include <stdio.h>  //standard input and output
 #define MAX = 89
 
@@ -12,6 +15,13 @@
 * See Makefile for what is happening in background
 */
 
+//this is main - it is what your program will always look for first
+//  main returns an integer.  If it reaches the bottom with no errors, 
+//  it will return 0, just like we told it
+//  if it does run into an error, it will automatically return 1, to 
+//  indictate the program had a run-time error
+//  main does not expect input, as indicated by the word void.  You could leave 
+//  it blank, but it is good practice to be verbose
 int main (void) {
     //integers only do whole numbers
     int i = 9;
