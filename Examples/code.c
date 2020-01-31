7ed8daef1578fa8336aab5c14866f6f3ec9d40cd
pattonsgirl <pattonsgirl@gmail.com>
2020-01-31 18:11:09 -0500

    emptied class code

--- Examples/code.c
+++ Examples/code.c
@@ -11,35 +11,6 @@
 */
 
 int main (void) {
-/*
-Write a basic guessing game.​
-Create a variable called answer​
-Create a variable called guess​
-Ask the user for a guess between 1 and 100​
-    Check if the guess is equal to the answer​
-    Check if the guess is within 5 digits of the correct answer​
-        Print getting hot!​
-    If the guess is more than 5 digits of the correct answer​
-        Print getting cold!​
-If the guess was out of bounds, print an error message​
-*/
-    int time = 0;
-    scanf("%d", &time);
-    //if time is between 0 /exclusively/ and 12 /exclusively/, print something
-    if(0 < time && time < 12){
-        printf("Good morning");
-    }
-    //otherwise if time is between 12 /inclusively/ and 18 /inclusively/, print something
-    else if (12 <= time && time <= 18){
-        printf("Good afternoon");
-    }
-    //otherwise if time is between 18 /inclusively/ and 24 /inclusively/, print something
-    else if(18 <= time && time <= 24){
-        printf("Good night!");
-    }
-    else {
-        printf("Time entered was not valid.");
-    }
-
+    
     return 0;
 }
