696ef9ece9c08282570d76885318112dcebd0559
pattonsgirl <pattonsgirl@gmail.com>
2020-02-07 15:31:56 -0500

    class code 2/7

--- Examples/code.c
+++ Examples/code.c
@@ -1,18 +1,8 @@
 #include <stdio.h>  //standard input and ouput
 #include <stdlib.h>  //needed to flush buffer
 
-/*
-*
-* To run:
-* Use shortcut Ctrl + Shift + C to bring up a cmd terminal
-* If you are using mingw 32 bit, use command: mingw32-make run
-* If you are using mingw 64 bit (or have changed mingw32-make to make) 
-*    use command: make run
-* See Makefile for what is happening in background
-*/
-
 int main (void) {
-    //owe a better example of breaks vs continues
+    
 
     return 0;
 }
---
+++ Examples/quiz3code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  //standard input and ouput
+#include <stdlib.h>  //needed to flush buffer
+
+int main (void) {
+    //owe a better example of breaks vs continues
+
+    for (int i = 6; i <= 16; i += 2){
+        printf("%d ", i);
+    }
+
+    printf("\n");
+    
+    int num = 15;
+    while(num > 10){
+        num--;
+        printf("%d ", num);
+    }
+
+    return 0;
+}
