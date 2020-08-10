1600ef4b46b9f04f3c11b29775c286e018c880ab
pattonsgirl <pattonsgirl@gmail.com>
2020-08-10 12:17:56 -0400

    commit cleanup

--- Examples/code.c
+++ Examples/code.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h> // to get malloc, calloc, free, and realloc
-#include <math.h>
-#include <string.h>
+#define MAX_NAME 15
+#define MAX_VEHICLES 5
 
-int main (void){   
-    
+typedef struct {
+    char make[MAX_NAME];
+    char model[MAX_NAME];
+    int year;
+    double mileage;
+} vehicle_t;
+
+typedef struct {
+    vehicle_t motors [MAX_VEHICLES];
+} garage_t;
+
+//void read_info (garage_t *);
+void print_all (garage_t *);
+
+int main (void){ 
+    garage_t my_garage; 
+    //read_info (&my_garage);
+    print_all (&my_garage);    
     return 0;
+}
+
+void print_all (garage_t *g){
+    for (int i = 0; i < MAX_VEHICLES; i++){
+        printf("%s: %s %d %lf\n", g->motors[i].make, g->motors[i].model, 
+            g->motors[i].year, g->motors[i].mileage);
+    }
 }
 
 
